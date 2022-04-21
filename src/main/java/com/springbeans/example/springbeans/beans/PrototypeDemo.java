5355311dd34318e412a329ba74ff90b8b48cd24d
saunaik19 <saunaik19@gmail.com>
2022-04-22 01:49:26 +0530

    code refinement

--- src/main/java/com/springbeans/example/springbeans/beans/PrototypeDemo.java
+++ src/main/java/com/springbeans/example/springbeans/beans/PrototypeDemo.java
@@ -1,10 +1,12 @@
 package com.springbeans.example.springbeans.beans;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope("prototype")
+//@Scope("prototype")
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)//correct way
 public class PrototypeDemo {
     public PrototypeDemo() {
         System.out.println("PrototypeDemo constructor called.....");
---
+++ src/main/java/com/springbeans/example/springbeans/questions/Student.java
@@ -0,0 +1,12 @@
+package com.springbeans.example.springbeans.questions;
+
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
+public class Student {
+    private String studentName;
+
+}
